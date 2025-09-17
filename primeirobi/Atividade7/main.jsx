4e797ae1f04641be84dc69cec5bd25b875853c71
kauangiacomin <kmgiacomin@minha.fag.edu.br>
2025-09-17 19:49:08 -0300

    .

--- primeirobi/Atividade7/main.jsx
+++ primeirobi/Atividade7/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // se estiver usando Tailwind ou seus estilos globais
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
